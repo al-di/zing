50b2ea2e3386d06a12fbd889e471a55b59973260
Jakob Hilden <jakobhilden@gmail.com>
2016-06-11 18:02:00 +0200

    add modal

--- www/js/app.js
+++ www/js/app.js
@@ -3,35 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('zing', ['ionic'])
 
-.controller('StundenplanCtrl', function($scope,$ionicModal) {
-  $ionicModal.fromTemplateUrl('my-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
+.controller('IndexCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/add.html', {
+    scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  // Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-
-    // Execute action
-  });
+  })
 })
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
